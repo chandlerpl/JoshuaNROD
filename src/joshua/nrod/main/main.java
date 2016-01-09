6bb34f21aa4dca60cf344761c884c9ac1178f53a
joshuapopelewis <j.popelewis@icloud.com>
2016-01-09 18:18:07 +0000

    Added Branding Testing upload feature.
    Change-Id: I926786356b40472466b61017e222cbeb04f88561
    Change-Id: JPL001
    Signed-off-by: joshuapopelewis <j.popelewis@icloud.com>

--- src/joshua/nrod/main/main.java
+++ src/joshua/nrod/main/main.java
@@ -4,32 +4,23 @@ import joshua.nrod.stomp.Client;
 import joshua.nrod.stomp.Listener;
 
 public class main {
-    
-    
-    //Network Rail ActiveMQ server
+
     private static final String SERVER = "datafeeds.networkrail.co.uk";
-    
-    // Server port for STOMP clients
+
     private static final int PORT = 61618;
-    
-    // Your account username, typically an email address
+   
     private static final String USERNAME = "joshuapopelewis@gmail.com";
-    
-    // Your account password
+
     private static final String PASSWORD = "11985Jpl!";
     
-    // Example topic (this one is for Southern Train Movements)
     private static final String TOPIC = "/topic/TD_LNW_C_SIG_AREA";
     
     public static void main(String[] args) throws Exception {
         new main().go();
     }
     
-    /*
-     * Connect to a single topic and subscribe a listener
-     * @throws Exception Too lazy to implement exception handling....
-     */
     public void go() throws Exception {
+    	System.out.println("| JPLogics - NROD TD Connection...");
         System.out.println("| Connecting...");
         Client client = new Client(SERVER, PORT, USERNAME, PASSWORD);
         if (client.isConnected()) {
