18d0b23a2017cce50782b3cb6ac0a894834ea487
Chandler <c.popelewis@gmail.com>
2016-01-09 17:30:45 +0000

    Fixed a few bugs

--- src/joshua/nrod/main/MyListener.java
+++ src/joshua/nrod/main/MyListener.java
@@ -8,35 +8,35 @@ import java.util.Map;
 import joshua.nrod.stomp.Listener;
 
 public class MyListener implements Listener {
+	
 	public void message(Map header, String body) {
     
-        System.out.println("| Message Recieved: " + header);
-        
-      	final List<String> ListBody = new ArrayList<String>();
-           
-      	int s = 0;
+		System.out.println("| Message Recieved: " + header);
+
+		final List<String> ListBody = new ArrayList<String>();
+   
+		int s = 0;
       	
-      	System.out.println("| Message Recieved: " + body);
-              	
-      	while(s != -1) {
-      		int t = body.indexOf(".");
-      		s = body.indexOf("},{");
-      		
-      		ListBody.add(body.substring(t+1, s+1));
-      		    		
-      		body = body.substring(s+2);
-      	}
-      	
-      	ListBody.add(body);
-      	
-       for(int i = 0; i < ListBody.size(); i++) {
-    	   if(ListBody.get(i).length() == 0) {
-    		   ListBody.remove(i);
-    	   }
-    	   
-    		System.out.println("| Message Recieved: " + ListBody.get(i));
+		while(s != -1) {
+			int t = body.indexOf(".");
+			s = body.indexOf("},{");
+  		   
+			if(s == -1) {
+				body = body.substring(s+1, body.length()-1);
+				ListBody.add(body);
+			} else {
+				if(body.contains("[")) {
+					ListBody.add(body.substring(t+2, s+1));
+					body = body.substring(s+2);
+				} else {
+					ListBody.add(body.substring(t+1, s+1));
+					body = body.substring(s+2);
+				}
+			}
+		}
+  	
+		for(int i = 0; i < ListBody.size(); i++) {
+	   		System.out.println("| Message Recieved: " + ListBody.get(i));
        }
     }
 }
-
-
