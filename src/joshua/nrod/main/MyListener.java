557bd2e752c49e52c95df9921c0fdf29b61ad51e
joshuapopelewis <j.popelewis@icloud.com>
2016-01-10 21:25:33 +0000

    [WIP] Decoding Messages
    Signed-off-by: joshuapopelewis <j.popelewis@icloud.com>

--- src/joshua/nrod/main/MyListener.java
+++ src/joshua/nrod/main/MyListener.java
@@ -36,50 +36,102 @@ public class MyListener implements Listener {
 		}
   	
 		 for(int i = 0; i < ListBody.size(); i++) {
-			 int msg_type = ListBody.get(i).indexOf("msg_type");
-			 	String msg_type1 = ListBody.get(i).substring(msg_type+11, msg_type+13);
-			 int SF = ListBody.get(i).indexOf("SF_MSG");
-			 	String SF1 = ListBody.get(i).substring(SF, SF+6);
-			 int SG = ListBody.get(i).indexOf("SG_MSG");
-			 	String SG1 = ListBody.get(i).substring(SG, SG+6);
-			 int SH = ListBody.get(i).indexOf("SH_MSG");
-			 	String SH1 = ListBody.get(i).substring(SH, SH+6);
-			 int CA = ListBody.get(i).indexOf("CA_MSG");
-			 	String CA1 = ListBody.get(i).substring(CA, CA+6);
-			 int CB = ListBody.get(i).indexOf("CB_MSG");
-			 	String CB1 = ListBody.get(i).substring(CB, CB+6);
-			 int CC = ListBody.get(i).indexOf("CC_MSG");
-			 	String CC1 = ListBody.get(i).substring(CC, CC+6);
-			 int CT = ListBody.get(i).indexOf("CT_MSG");
-			 	String CT1 = ListBody.get(i).substring(CT, CT+6);
-			 	
-			 	
-			 	
-			 int descr = ListBody.get(i).indexOf("descr");
-			 	String descr1 = ListBody.get(i).substring(descr+8, descr+12);
-			 int to = ListBody.get(i).indexOf("to");
-			 	String to1 = ListBody.get(i).substring(to+5, to+9);
-			 int from = ListBody.get(i).indexOf("from");
-			 	String from1 = ListBody.get(i).substring(from+7, from+11);
-			 int area_id = ListBody.get(i).indexOf("area_id");
-			 	String area_id1 = ListBody.get(i).substring(area_id+10, area_id+12);
-			// int time = ListBody.get(i).indexOf("time");
-			 //	String time1 = ListBody.get(i).substring(time+7, time+20);
-			// int address = ListBody.get(i).indexOf("address");
-			 //	String address1 = ListBody.get(i).substring(address+10, address+12);
-			// int data = ListBody.get(i).indexOf("data");
-			//	String data1 = ListBody.get(i).substring(data+7, data+15);
-			// int report_time = ListBody.get(i).indexOf("report_time");
-			//	String report_time1 = ListBody.get(i).substring(report_time+7, report_time+20);
-			 
-			 System.out.println(msg_type1);
-			 System.out.println("| " + SF1);
-			 System.out.println("| " + SG1);
-			 System.out.println("| " + SH1);
-			 System.out.println(" " + CA1 + ": Consist " + descr1 + " moved from " + from1 + " to " + to1 + " in area " + area_id1);
-			 System.out.println("| " + CB1);
-			 System.out.println("| " + CC1);
-			 System.out.println("| " + CT1);
+			// int msg_type = ListBody.get(i).indexOf("msg_type");
+			 //	String msg_type1 = ListBody.get(i).substring(msg_type+11, msg_type+13);
+		 
+			 if(body.contains("SF")) {
+				int SF = ListBody.get(i).indexOf("SF_MSG");
+				 	String SF1 = ListBody.get(i).substring(SF, SF+6);
+				int time = ListBody.get(i).indexOf("time");
+					String time1 = ListBody.get(i).substring(time+7, time+20);	
+				int area_id = ListBody.get(i).indexOf("area_id");
+				 	String area_id1 = ListBody.get(i).substring(area_id+10, area_id+12);	
+				int address = ListBody.get(i).indexOf("address");
+					String address1 = ListBody.get(i).substring(address+10, address+12); 	
+				int data = ListBody.get(i).indexOf("data");
+					String data1 = ListBody.get(i).substring(data+7, data+15); 	
+				System.out.println(SF1 + ":Signal in area " + area_id1 + "was updated. (" + address1 + ", " + data1 + ", " + time1 + ")");	
+			 }
+			 else if(body.contains("SG")) {
+				int SG = ListBody.get(i).indexOf("SG_MSG");
+				 	String SG1 = ListBody.get(i).substring(SG, SG+6);
+				int time = ListBody.get(i).indexOf("time");
+					String time1 = ListBody.get(i).substring(time+7, time+20);	
+				int area_id = ListBody.get(i).indexOf("area_id");
+				 	String area_id1 = ListBody.get(i).substring(area_id+10, area_id+12);	
+				int address = ListBody.get(i).indexOf("address");
+					String address1 = ListBody.get(i).substring(address+10, address+12); 	
+				int data = ListBody.get(i).indexOf("data");
+					String data1 = ListBody.get(i).substring(data+7, data+15); 	
+				System.out.println(SG1 + ": Signal in area " + area_id1 + "was refreshed. (" + address1 + ", " + data1 + ", " + time1 + ")");	 
+			}
+			 else if(body.contains("SH")) {
+					int SH = ListBody.get(i).indexOf("SH_MSG");
+					 	String SH1 = ListBody.get(i).substring(SH, SH+6);
+					int time = ListBody.get(i).indexOf("time");
+						String time1 = ListBody.get(i).substring(time+7, time+20);	
+					int area_id = ListBody.get(i).indexOf("area_id");
+					 	String area_id1 = ListBody.get(i).substring(area_id+10, area_id+12);	
+					int address = ListBody.get(i).indexOf("address");
+						String address1 = ListBody.get(i).substring(address+10, address+12); 	
+					int data = ListBody.get(i).indexOf("data");
+						String data1 = ListBody.get(i).substring(data+7, data+15); 	
+					System.out.println(SH1 + ": Signal in area " + area_id1 + "was Finished. (" + address1 + ", " + data1 + ", " + time1 + ")");	
+			}
+			 else if(body.contains("CA")) {
+				 	int CA = ListBody.get(i).indexOf("CA_MSG");
+				 		String CA1 = ListBody.get(i).substring(CA, CA+6);
+				 	int time = ListBody.get(i).indexOf("time");
+						String time1 = ListBody.get(i).substring(time+7, time+20);	
+					int area_id = ListBody.get(i).indexOf("area_id");
+						String area_id1 = ListBody.get(i).substring(area_id+10, area_id+12);
+					int descr = ListBody.get(i).indexOf("descr");
+					 	String descr1 = ListBody.get(i).substring(descr+8, descr+12);
+					int to = ListBody.get(i).indexOf("to");
+					 	String to1 = ListBody.get(i).substring(to+5, to+9);
+					int from = ListBody.get(i).indexOf("from");
+					 	String from1 = ListBody.get(i).substring(from+7, from+11);
+					System.out.println(CA1 + ": Consist " + descr1 + " in area " + area_id1 + " moved from " + from1 + " to " + to1 + " at " + time1);
+			 }	
+			 else {
+				 System.out.println("A message that has not yet been decoded has arrived.");
+			 }
+						
+						
+						
+						
+			//	 int CB = ListBody.get(i).indexOf("CB_MSG");
+			//	 	String CB1 = ListBody.get(i).substring(CB, CB+6);
+			//	 int CC = ListBody.get(i).indexOf("CC_MSG");
+			//	 	String CC1 = ListBody.get(i).substring(CC, CC+6);
+			//	 int CT = ListBody.get(i).indexOf("CT_MSG");
+			//	 	String CT1 = ListBody.get(i).substring(CT, CT+6);
+				
+				 	
+				 	
+			//	 int descr = ListBody.get(i).indexOf("descr");
+			//	 	String descr1 = ListBody.get(i).substring(descr+8, descr+12);
+			//	 int to = ListBody.get(i).indexOf("to");
+			//	 	String to1 = ListBody.get(i).substring(to+5, to+9);
+			//	 int from = ListBody.get(i).indexOf("from");
+			//	 	String from1 = ListBody.get(i).substring(from+7, from+11);
+			//	 int area_id = ListBody.get(i).indexOf("area_id");
+			//	 	String area_id1 = ListBody.get(i).substring(area_id+10, area_id+12);
+				// int time = ListBody.get(i).indexOf("time");
+				 //	String time1 = ListBody.get(i).substring(time+7, time+20);
+				// int address = ListBody.get(i).indexOf("address");
+				 //	String address1 = ListBody.get(i).substring(address+10, address+12);
+				// int data = ListBody.get(i).indexOf("data");
+				//	String data1 = ListBody.get(i).substring(data+7, data+15);
+				// int report_time = ListBody.get(i).indexOf("report_time");
+				//	String report_time1 = ListBody.get(i).substring(report_time+7, report_time+20); 
+		
+					// System.out.println("| " + SG1);
+					 //System.out.println("| " + SH1);
+					 //System.out.println(" " + CA1 + ": Consist " + descr1 + " moved from " + from1 + " to " + to1 + " in area " + area_id1);
+					 //System.out.println("| " + CB1);
+					 //System.out.println("| " + CC1);
+					// System.out.println("| " + CT1);
 			 
        }
     }
