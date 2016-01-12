9abcf42670e3749687c14982ce80862e11c7c782
joshuapopelewis <j.popelewis@icloud.com>
2016-01-12 21:49:42 +0000

    [Updated] New feeds added.

--- src/joshua/nrod/main/MyFeeds.java
+++ src/joshua/nrod/main/MyFeeds.java
@@ -4,7 +4,9 @@ public class MyFeeds {
 	
 	static final String TOPIC_SIG_AREA = "TD_LNW_C_SIG_AREA";
 	static final String TOPIC_MVT_TOC = "TRAIN_MVT_HE_TOC";
+	static final String TOPIC_RTPM_ALL = "RTPPM_ALL";
 	static final String TOPIC_SIG = "/topic/"+ TOPIC_SIG_AREA;
 	static final String TOPIC_MVT = "/topic/"+ TOPIC_MVT_TOC;
+	static final String TOPIC_RTPM = "/topic/"+ TOPIC_RTPM_ALL;
 
 }
