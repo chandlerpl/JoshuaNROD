95ec2ea26452e68eee0596ca7f21f63f15d02d74
joshuapopelewis <j.popelewis@icloud.com>
2016-01-12 16:52:18 +0000

    [New Class] Feed Streams are now in this file.
    Signed-off-by: joshuapopelewis <j.popelewis@icloud.com>

---
+++ src/joshua/nrod/main/MyFeeds.java
@@ -0,0 +1,10 @@
+package joshua.nrod.main;
+
+public class MyFeeds {
+	
+	static final String TOPIC_SIG_AREA = "TD_LNW_C_SIG_AREA";
+	static final String TOPIC_MVT_TOC = "TRAIN_MVT_HE_TOC";
+	static final String TOPIC_SIG = "/topic/"+ TOPIC_SIG_AREA;
+	static final String TOPIC_MVT = "/topic/"+ TOPIC_MVT_TOC;
+
+}
