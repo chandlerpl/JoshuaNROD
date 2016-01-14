de0ff82050beb316e4f41e0c9268bcb920993a33
joshuapopelewis <j.popelewis@icloud.com>
2016-01-14 12:14:33 +0000

    [Updated] Now includes TSR/RTPM Feeds for future development.
    Signed-off-by: joshuapopelewis <j.popelewis@icloud.com>

--- src/joshua/nrod/main/MyFeeds.java
+++ src/joshua/nrod/main/MyFeeds.java
@@ -4,7 +4,12 @@ public class MyFeeds {
 	
 	static final String TOPIC_SIG_AREA = "TD_LNW_C_SIG_AREA";
 	static final String TOPIC_MVT_TOC = "TRAIN_MVT_HE_TOC";
+	static final String TOPIC_RTTPM_ALL = "RTTPM_ALL";
+	static final String TOPIC_TSR_AREA = "TSR_LNW_N_ROUTE";
 	static final String TOPIC_SIG = "/topic/"+ TOPIC_SIG_AREA;
 	static final String TOPIC_MVT = "/topic/"+ TOPIC_MVT_TOC;
+	static final String TOPIC_RTTPM = "/topic/" + TOPIC_RTTPM_ALL;
+	static final String TOPIC_TSR = "/topic/" + TOPIC_TSR_AREA;
+	
 	
 }
