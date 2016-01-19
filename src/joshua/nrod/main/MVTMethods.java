f8db6b4e43759e5a7e1ce043d890ef9a7b2505e7
joshuapopelewis <j.popelewis@icloud.com>
2016-01-19 09:49:16 +0000

    [Updated] Code to fill empty tag:val statements. Val now = na.
    Signed-off-by: joshuapopelewis <j.popelewis@icloud.com>

--- src/joshua/nrod/main/MVTMethods.java
+++ src/joshua/nrod/main/MVTMethods.java
@@ -83,9 +83,12 @@ public class MVTMethods {
 	public static void MVT_0003_MSG(String MVT_MSG) {
 
 		 MVT_MSG = MVT_MSG.replaceAll("\\{", "");   // strip {
+		 	MVT_MSG = MVT_MSG.replaceAll(":''", ":'na'");
 			MVT_MSG = MVT_MSG.replaceAll("\\}", "");  // strip }
 			MVT_MSG = MVT_MSG.replaceAll("\'", "");  //strip single quotes
-			MVT_MSG = MVT_MSG.replaceAll("MVT_MSG:",""); //strip the header   
+			MVT_MSG = MVT_MSG.replaceAll("MVT_MSG:",""); //strip the header
+			
+			System.out.println(MVT_MSG);
 			//time:1452473490000,area_id:SK,address:71,msg_type:SF,data:EA   -- what the new string is!
 			 String[] mvtContent3 = MVT_MSG.split(",");  // split each value at the commas
 			 
@@ -101,28 +104,27 @@ public class MVTMethods {
 			  String tag = mvtLine3[0];
 			  String val = mvtLine3[1];
 			  
-			  if(val.isEmpty()) {
-				   } else {
 			  if(tag.equalsIgnoreCase("msg_type")) {
 				  msg_type = val;
-			  } else if(tag.equalsIgnoreCase("toc_id")) {
-				  toc_id = val;
-			  } else if(tag.equalsIgnoreCase("report_time")) {
-				  report_time = val;
-			  } else if(tag.equalsIgnoreCase("next_report_stanox")) {
-				  next_report_stanox = val;
-			  } else if(tag.equalsIgnoreCase("loc_stanox")) {
-				  loc_stanox = val;
-			  } else if(tag.equalsIgnoreCase("train_service_code")) {
-				  train_service_code = val;
-			  }	
-			 }
-			 }
+			  }  else if(tag.equalsIgnoreCase("toc_id")) {
+					   toc_id = val;
+			  }  else if(tag.equalsIgnoreCase("report_time")) {
+					   report_time = val;
+			  }  else if(tag.equalsIgnoreCase("next_report_stanox")) {
+					   next_report_stanox = val;
+			  }  else if(tag.equalsIgnoreCase("loc_stanox")) {
+					   loc_stanox = val;
+			  }  else if(tag.equalsIgnoreCase("train_service_code")) {
+					   train_service_code = val;
+				   
+			  	}
+			  }
+			 
 			 java.util.Date report_time1 = new java.util.Date(Long.parseLong(report_time));
 
 			 System.out.println("MVT_MSG " + msg_type + ": Train "+ train_service_code + " operated by TOC " + toc_id +" reported at " + loc_stanox + " next report at "+ next_report_stanox + ". Report sent at " +report_time1 );
 			 
-		 	}
+	}
 	public static void MVT_0004_MSG(String MVT_MSG) {
 		int MVTType = MVT_MSG.indexOf("msg_type");
  		String MVT1 = MVT_MSG.substring(MVTType+11, MVTType+15);
